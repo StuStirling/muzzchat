70e611697d4b81bf16e8a0558af80cd3f4eaa551
Stu Stirling <stuart.stirling@glintpay.com>
2023-08-10 22:01:25 +0100

    Initial commit

---
+++ app/src/main/java/com/stustirling/muzzchat/MuzzChatApplication.kt
@@ -0,0 +1,8 @@
+package com.stustirling.muzzchat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MuzzChatApplication : Application() {
+}
---
+++ app/src/main/java/com/stustirling/muzzchat/feature/chat/ChatScreen.kt
@@ -0,0 +1,19 @@
+package com.stustirling.muzzchat.feature.chat
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+internal fun ChatRoute(
+    onNavigateUp: () -> Unit,
+    viewModel: ChatViewModel = viewModel(),
+) {
+
+}
+
+@Composable
+private fun ChatScreen(
+
+) {
+
+}
